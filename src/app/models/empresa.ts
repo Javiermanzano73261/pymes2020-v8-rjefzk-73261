a28522ad538dca9742a3a2da85fb63c072bb7f47
Javiermanzano73261 <manzanojavier@hotmail.com>
2020-11-09 15:42:05 -0300

    1 file added

---
+++ src/app/models/empresa.ts
@@ -0,0 +1,13 @@
+export class Empresa {
+  IdEmpresa: number;
+  Nombre: string;
+  FechaFundacion: string;
+  CantidadEmpleados: number;
+
+
+  /**IdEmpresa (pk autoincremental)
+Nombre: char(50)   “ojo que tiene tamaño fijo, se autocompleta con espacios”
+FechaFundacion: Date
+CantidadEmpleados: int 
+*/
+}
